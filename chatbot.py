258ff3a7761a39db3e2d4720b101310df9c8c637
Hemant Rajput <104062143+hemantrajputt@users.noreply.github.com>
2025-04-18 15:55:08 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,10 +8,10 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEndpoint
 
-DB_FAISS_PATH="VectorEmbeddings/db_faiss"
+DB_FAISS_PATH = "VectorEmbeddings/db_faiss"
 @st.cache_resource
 def get_vectorstore():
-    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
     db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
     return db
 
